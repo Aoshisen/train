176d387d7e64054d9e3609513aac809c86f6f72d
aoshien <1041370212@qq.com>
2021-08-09 10:05:27 +0800

    打包路径修改

---
+++ 05/src/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDom from "react-dom"
+import App from "./App"
+import {BrowserRouter} from "react-router-dom"
+ReactDom.render(
+    <BrowserRouter>
+    <App/>
+    </BrowserRouter>
+,document.getElementById('root'))
---
+++ 05/src/routes.js
@@ -0,0 +1,11 @@
+const routes =[
+    {
+        to:"popular",
+        content:"Popular",
+    },{
+        to:"battle",
+        content:"Battle"
+    }
+]
+const secondMenu=["All","JavaScript","Ruby","Java","CSS","Python"];
+export  {routes,secondMenu};
